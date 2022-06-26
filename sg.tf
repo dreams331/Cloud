e8b0bc40b88dea41d2e060fcddbda4b389042ef6
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-26 13:58:35 +0100

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -52,3 +52,28 @@ resource "aws_security_group" "elb_http" {
     Name = "createdby-akeem"
   }
 }
+
+
+resource "aws_security_group" "db-sg" {
+  name        = "main_rds_sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "createdby-akeem"
+  }
+}
