39749613ff8b7d7d6e0d809721879e530d02fb7f
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-26 13:40:32 +0100

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,54 @@
+
+# I describe the Security Group for our web-servers, which will allow HTTP connections to our instances
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound connections"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "createdby-akeem"
+  }
+}
+
+
+
+# I create a elb and a security group for my elb
+
+resource "aws_security_group" "elb_http" {
+  name        = "elb_http"
+  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "createdby-akeem"
+  }
+}
