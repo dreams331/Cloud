2458ad4af7a591b82e0dae262e44004057e32ccf
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-26 13:46:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,39 +119,6 @@ resource "aws_launch_configuration" "web" {
 
 
 
-resource "aws_elb" "web_elb" {
-  name = "web-elb"
-  security_groups = [
-    aws_security_group.elb_http.id
-  ]
-  subnets = [
-    aws_subnet.public_eu_north_1a.id,
-    aws_subnet.public_eu_north_1b.id
-  ]
-
-  cross_zone_load_balancing   = true
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:80/"
-  }
-
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "80"
-    instance_protocol = "http"
-  }
-
-}
-
-
-output "elb_dns_name" {
-  value = aws_elb.web_elb.dns_name
-}
 
 
 
