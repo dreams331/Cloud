8101cb785fae2864b01f515199b44050af952bd1
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-07-21 14:01:18 +0100

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -4,6 +4,8 @@ resource "aws_db_instance" "default" {
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
+  db_subnet_id           = "aws_subnet.eu_north_1c.id"
+  availability_zone      = "eu-north-1b"
   name                   = "mydb"
   username               = "foo"
   password               = "foobarbaz"
